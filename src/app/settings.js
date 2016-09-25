ba057f4980190186bfba10db9a80d5da1be9aa84
Matt Hampel <matth@localdata.com>
2016-09-25 12:04:15 -0400

    Update 990 endpoint

--- src/app/settings.js
+++ src/app/settings.js
@@ -1,7 +1,7 @@
 var settings = {
   api: {
     baseurl: 'https://data.detroitledger.org/api/1.0',
-    financeAPI: 'http://localhost:3000',
+    financeAPI: 'https://data.detroitledger.org:6001',
     dateFormat: 'ddd, DD MMM YYYY HH:mm:ss ZZ'
   }
 };
