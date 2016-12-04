4ae66b661ebba242ca8149759ed2926054383bdb
Matt Hampel <matth@localdata.com>
2016-12-04 11:23:40 -0500

    Switch 991 api endpoint

--- src/app/settings.js
+++ src/app/settings.js
@@ -1,7 +1,7 @@
 var settings = {
   api: {
     baseurl: 'https://data.detroitledger.org/api/1.0',
-    financeAPI: 'https://data.detroitledger.org:6001',
+    financeAPI: 'https://data.detroitledger.org/990api',
     dateFormat: 'ddd, DD MMM YYYY HH:mm:ss ZZ'
   }
 };
