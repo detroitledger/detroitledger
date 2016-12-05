a739d86de865f2ed141296cf4b0882e7bf1ef111
Benjamin Chodoroff <bc@thermitic.net>
2016-12-05 10:24:35 -0500

    use Olark instead of flag (#105)
    * enable single-page app mode in olark
    
    this is not required for this work, i just noticed it was an option we should probably use
    it will notify the agent if a visitor changes pages, in theory
    
    * instead of flags, use olark to report problems

--- src/app/views/flag.js
+++ src/app/views/flag.js
@@ -2,24 +2,22 @@ var fs = require('fs'),
     $ = require('jquery'),
     _ = require('lodash'),
     Backbone = require('backbone'),
-    Flags = require('../models/flags'),
     template = require('../templates/flag.html');
 
+/**
+ * Render a "report problems" button that will, if olark is loaded,
+ * open up the chat box, or if not, prompt user to just send an email.
+ */
 
   var PageView = Backbone.View.extend({
     el: '#flag',
     events: {
       'click .flag-toggle': 'show',
-      'click .submit': 'submit'
     },
     template: template,
 
     initialize: function(options) {
-      _.bindAll(this, 'render', 'show', 'submit', 'error', 'success');
-
-      this.flag = new Flags.Model({
-        target_id: options.target_id
-      });
+      _.bindAll(this, 'render', 'show');
       this.render();
     },
 
@@ -29,28 +27,13 @@ var fs = require('fs'),
       }));
     },
 
-    error: function(error) {
-      console.log("Unable to save response", error);
-      this.$el.find('.error').fadeIn(400);
-    },
-
-    success: function(response) {
-      this.$el.find('.thanks').fadeIn(400);
-    },
-
-    submit: function(event) {
-      event.preventDefault();
-      var text = $('#flag textarea').val();
-      this.flag.set('note', text);
-      this.flag.save();
-
-      this.flag.on('error', this.error);
-      this.flag.on('sync', this.success);
-    },
-
     show: function(event) {
       event.preventDefault();
-      $('#flag .panel-body').show();
+      if (typeof olark === 'function') {
+        olark('api.box.expand');
+      } else {
+        $('#flag .panel-body').show();
+      }
     }
   });
 
